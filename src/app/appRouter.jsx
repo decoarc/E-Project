86dca2c00dea6e8ecbb36950d476cb88b96f89d7
decarc <anderarcodaci@gmail.com>
2026-05-01 23:27:07 -0300

    feat: wire checkout routes with CheckoutLayout and CheckoutGuard

--- src/app/appRouter.jsx
+++ src/app/appRouter.jsx
@@ -6,6 +6,12 @@ import Product from "../pages/product/product";
 import Cart from "../pages/cart/cart";
 import Stories from "../pages/stories/stories";
 import NotFound from "../pages/notFound";
+import CheckoutLayout from "../components/checkout/checkoutLayout";
+import CheckoutGuard from "../components/checkout/checkoutGuard";
+import CartReview from "../pages/checkout/cartReview";
+import Shipping from "../pages/checkout/shipping";
+import Payment from "../pages/checkout/payment";
+import Confirmation from "../pages/checkout/confirmation";
 
 export default function AppRouter() {
   return (
@@ -20,6 +26,40 @@ export default function AppRouter() {
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/stories" element={<Stories />} />
+        <Route element={<CheckoutLayout />}>
+          <Route
+            path="/checkout/cart"
+            element={
+              <CheckoutGuard step="cart">
+                <CartReview />
+              </CheckoutGuard>
+            }
+          />
+          <Route
+            path="/checkout/shipping"
+            element={
+              <CheckoutGuard step="shipping">
+                <Shipping />
+              </CheckoutGuard>
+            }
+          />
+          <Route
+            path="/checkout/payment"
+            element={
+              <CheckoutGuard step="payment">
+                <Payment />
+              </CheckoutGuard>
+            }
+          />
+          <Route
+            path="/checkout/confirmation"
+            element={
+              <CheckoutGuard step="confirmation">
+                <Confirmation />
+              </CheckoutGuard>
+            }
+          />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
